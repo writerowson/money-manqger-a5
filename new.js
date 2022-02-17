77a61346c6822b0c7c553e6cd67beb5a094625a4
coderowson <runurowson@gmail.com>
2022-02-17 22:37:11 +0600

    error handling

--- new.js
+++ new.js
@@ -13,17 +13,14 @@ function sum(food, rent, cloth) {
     let total = Number(food) + Number(rent) + Number(cloth);
     return total
 }
-
-function updateBalance(balancee1, balancee2) {
+function updateBalance(balance1, balance2) {
     // if (typeof income != 'number' && typeof expense >= '0') {
     //     return "please input a valid number"
     // }
-    let result = Number(balancee1) - Number(balancee2)
+    let result = Number(balance1) - Number(balance2)
     return result;
 }
-
 function calculation() {
-
     let sumOfExpenses = sum(foodExpenses.value, rentExpenses.value, clothExpenses.value)
     totalExpenses.innerText = sumOfExpenses;
 
@@ -41,16 +38,10 @@ function saving(incomeAmount, savingPercentage) {
     let savingInput = Number(incomeAmount) / 100 * Number(savingPercentage)
     return savingInput
 }
-function remainingBalance(balance1, balance2) {
-    let remainBalance = balance1 - balance2
-    // let remainBalance = Number(balance1) - Number(balance2)
-    return remainBalance
-}
-
 function finalCalculation() {
     let save = saving(incomeAmount.value, savingPercentage.value)
     savingAmount.innerText = save;
 
-    let lastBalance = remainingBalance(balanceOutput.innerText, savingAmount.innerText)
+    let lastBalance = updateBalance(balanceOutput.innerText, savingAmount.innerText)
     remainingBalanceOutput.innerText = lastBalance
 }
